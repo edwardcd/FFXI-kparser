a3dc8d095b3d3bc888fcea0b4e0a881ed5a08751
Poroburu <poroburu@gmail.com>
2023-01-17 22:58:29 -0500

    Update MessageLine.cs
    Timestamp prefix for Ashita V4

--- ParserCore/Parsing/MessageLine.cs
+++ ParserCore/Parsing/MessageLine.cs
@@ -53,6 +53,7 @@ namespace WaywardGamers.KParser
             @"((\x1e\x01)+(\x81\x40)?(\x1e\x01)*)?" +
             //@"(?<tsPlugin>((\x1e(\x3f|\xfa|\xfc)\[)|(\x1e.)|(\[))(?<time>\d{2}:\d{2}:\d{2})\s?(?<ampm>\w{2})?\] (\x1e\x01)?)?" +
             //@"(?<tsPlugin>((\x1e.)?\[)(?<time>\d{2}:\d{2}:\d{2})\s*(?<ampm>\w{2})?\]\s*\x1e\x01)?" +
+            @"(\u001eQ)?" +
             @"(?<tsPlugin>((\x1e.)?(\[)?)(?<time>\d{2}:\d{2}(:\d{2})?)\s*(?<ampm>\w{2})?(\])?\s*\x1e\x01)?" +
             @"(?<remainder>.+)$");
 
@@ -61,6 +62,7 @@ namespace WaywardGamers.KParser
             @"(?<eventSeq>[0-9a-f]{8}),(?<uniqSeq>[0-9a-f]{8}),(?<strLen>[0-9a-f]{4}),(?<unk1>[0-9a-f]{2})," +
             @"(?<unk2>[0-9a-f]{2}),(?<msgCat>[0-9a-f]{2}),(?<xxxSeq1>[0-9a-f]{2}),(?<xxxSeq2>[0-9a-f]{8}),(?<xxxSeq3>[0-9a-f]{8}),(?<xxxSeq4>[0-9a-f]{8}),(?<xxxSeq5>[0-9a-f]{8}),(?<xxxSeq6>[0-9a-f]{8}),(?<xxxSeq7>[0-9a-f]{8}),(?<xxxSeq8>[0-9a-f]{8}),(?<xxxSeq9>[0-9a-f]{8}),(?<xxxSeq10>[0-9a-f]{8}),(?<unk3>[0-9a-f]{2})," +
             //@"(?<tsPlugin>\[?(?<time>\d{1,2}:\d{2}:\d{2})\s*(?<ampm>\w{2})?\]\s*)?" +
+            @"(\u001eQ)?" +
             @"(?<tsPlugin>(\[)?(?<time>\d{1,2}:\d{2}(:\d{2})?)\s*(?<ampm>\w{2})?(\])?\s*)?" +
             @"(?<remainder>.+)$");
 
